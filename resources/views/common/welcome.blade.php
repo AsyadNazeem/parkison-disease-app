5109b84399a3851eb79cc05d3133c46646bd3c2b
Asyad Nazeem <110421551+AsyadNazeem@users.noreply.github.com>
2025-01-20 22:16:58 +0530

    created login page

---
+++ resources/views/common/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+
+@section('content')
+
+<!-- Main Content -->
+<main class="container my-5">
+    <h1 class="text-center mb-4">Welcome to Parkinson Disease Detection App</h1>
+    <p class="text-center">This is where the main content goes. Add forms, tables, or other components here.</p>
+</main>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ParkinsonController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('common.welcome');
 });
 
-Route::resource('parkinson',ParkinsonController::class);
+Route::get('/register', [RegisterController::class, 'index']) -> name('index.register');
+Route::post('/register', [RegisterController::class, 'submit']) -> name('submit.register');
+
+Route::get('/login', [LoginController::class, 'index']) -> name('index.login');
+
+Route::resource('/parkinson',ParkinsonController::class);
